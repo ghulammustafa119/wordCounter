d2c747dc3b9fb7e539d1af6399a3dfabf97029db
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2024-05-05 13:20:45 +0500

    wordCounter

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "enter a sentence"
+    }
+]);
+let words = answers.sentence.trim().split(" ")
+console.log (words);
+console.log (`word count ${words.length}`);
